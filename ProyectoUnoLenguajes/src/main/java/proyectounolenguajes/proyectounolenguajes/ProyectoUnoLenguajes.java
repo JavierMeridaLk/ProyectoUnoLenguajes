1ab3cdbf0b95363e17e796b845d30157f4943e57
Javier Mérida <javiermerida202230638@cunoc.edu.gt>
2024-09-24 18:55:12 -0600

    Cargar archivo de netbeans

---
+++ ProyectoUnoLenguajes/src/main/java/proyectounolenguajes/proyectounolenguajes/ProyectoUnoLenguajes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyectounolenguajes.proyectounolenguajes;
+
+/**
+ *
+ * @author xavi
+ */
+public class ProyectoUnoLenguajes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
